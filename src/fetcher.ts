d7e0bacc9014699c9aafa7a9adc79e1c3ef1ec0a
leee62 <ashenone62@outlook.com>
2025-04-16 11:31:40 +0800

    feat: 🎸 get issues

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,4 +1,57 @@
-/** Fetch sentry issue by <ID> */
+/** get sentry event by <ID> */
+export async function fetchSentryEvent<T>(
+  eventId: string,
+  organization_id_or_slug: string,
+  project_id_or_slug: string
+): Promise<T | null> {
+  try {
+    const issueRes = await fetch(
+      `https://${process.env.SENTRY_HOST}/api/0/projects/${organization_id_or_slug}/${project_id_or_slug}/events/${eventId}/`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.SENTRY_USER_TOKEN}`,
+        },
+      }
+    );
+
+    if (!issueRes.ok) {
+      throw new Error(`HTTP error! status: ${issueRes.status}`);
+    }
+    return (await issueRes.json()) as T;
+  } catch (error) {
+    console.error("Error making request:", error);
+    return null;
+  }
+}
+
+/** get sentry events */
+export async function fetchSentryEvents<T>(
+  organization_id_or_slug: string,
+  project_id_or_slug: string
+): Promise<T | null> {
+  try {
+    const issueRes = await fetch(
+      `https://${process.env.SENTRY_HOST}/api/0/projects/${organization_id_or_slug}/${project_id_or_slug}/events/`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.SENTRY_USER_TOKEN}`,
+        },
+      }
+    );
+
+    if (!issueRes.ok) {
+      throw new Error(`HTTP error! status: ${issueRes.status}`);
+    }
+    return (await issueRes.json()) as T;
+  } catch (error) {
+    console.error("Error making request:", error);
+    return null;
+  }
+}
+
+/** get sentry issue by <ID> */
 export async function fetchSentryIssue<T>(
   issueId: string,
   organization_id_or_slug: string
@@ -24,7 +77,7 @@ export async function fetchSentryIssue<T>(
   }
 }
 
-/** Fetch sentry issue in <ALL> or <Project> */
+/** get sentry issue */
 export async function fetchSentryIssues<T>(
   project_id_or_slug: string,
   organization_id_or_slug: string
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import { fetchSentryIssue, fetchSentryIssues } from "./fetcher";
+import {
+  fetchSentryEvent,
+  fetchSentryEvents,
+  fetchSentryIssue,
+  fetchSentryIssues,
+} from "./fetcher";
 
 /** Create server instance */
 const server = new McpServer({
@@ -11,9 +16,123 @@ const server = new McpServer({
   version: "1.0.0",
 });
 
+/** Register sentry tool for getting single event by id */
+server.tool(
+  "get_single_event",
+  "get issue event by inputting sentry issue event url or sentry issue event id",
+  {
+    url_or_id: z
+      .string()
+      .describe("sentry issue event url or sentry issue event id"),
+    organization_id_or_slug: z
+      .string()
+      .optional()
+      .describe("sentry organization id or slug, it can be undefined"),
+    project_id_or_slug: z
+      .string()
+      .optional()
+      .describe("sentry project name or slug, it can be undefined"),
+  },
+  async ({
+    url_or_id,
+    organization_id_or_slug = process.env.SENTRY_ORG as string,
+    project_id_or_slug = process.env.SENTRY_PROJ as string,
+  }) => {
+    let EVENT_ID = "";
+
+    if (url_or_id.includes("http") || url_or_id.includes("https")) {
+      EVENT_ID = url_or_id.match(/events\/([a-f0-9]+)/)?.[1] || "";
+    } else {
+      EVENT_ID = url_or_id;
+    }
+
+    if (!EVENT_ID) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Invalid Event ID",
+          },
+        ],
+      };
+    }
+
+    const issueEventData = await fetchSentryEvent(
+      EVENT_ID,
+      organization_id_or_slug,
+      project_id_or_slug
+    );
+
+    if (!issueEventData) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Failed to Get Issue",
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(issueEventData),
+        },
+      ],
+    };
+  }
+);
+
+/** Register sentry tool for getting events (Watch out LLM TOKEN usage) */
+server.tool(
+  "get_project_events",
+  "get issue events by inputting sentry organization id or slug and sentry project name or slug",
+  {
+    organization_id_or_slug: z
+      .string()
+      .optional()
+      .describe("sentry organization id or slug, it can be undefined"),
+    project_id_or_slug: z
+      .string()
+      .optional()
+      .describe("sentry project name or slug, it can be undefined"),
+  },
+  async ({
+    organization_id_or_slug = process.env.SENTRY_ORG as string,
+    project_id_or_slug = process.env.SENTRY_PROJ as string,
+  }) => {
+    const issueEventData = await fetchSentryEvents(
+      organization_id_or_slug,
+      project_id_or_slug
+    );
+
+    if (!issueEventData) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Failed to Get Issue",
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(issueEventData),
+        },
+      ],
+    };
+  }
+);
+
 /** Register sentry tool for getting single issue by id */
 server.tool(
-  "get-issue",
+  "get_single_issue",
   "get issue by inputting sentry issue url or sentry issue id",
   {
     url_or_id: z.string().describe("sentry issue url or sentry issue id"),
@@ -69,9 +188,9 @@ server.tool(
   }
 );
 
-/** Register Sentry get multiple issues */
+/** Register Sentry get issues (Watch out LLM TOKEN usage) */
 server.tool(
-  "get-issues",
+  "get_project_issues",
   "get issues by inputting",
   {
     organization_id_or_slug: z
